772d5dadcacb9e542f7ec43eb3da33c5eedce939
Shaevitzaiden <Aidenashaevitz@gmail.com>
2025-03-31 11:57:31 -0700

    Calculating R^scores
    scores look pretty good, just need to create tools to export and import calibration coefficients

--- sensor_calibration/sensor_calibration_tool.py
+++ sensor_calibration/sensor_calibration_tool.py
@@ -8,6 +8,9 @@ sys.path.append(SENSOR_FILE_PATH)
 
 import numpy as np
 import pandas as pd
+from scipy.signal import savgol_filter
+import numpy.polynomial.polynomial as poly
+from sklearn.metrics import r2_score
 
 # base classes to make custom sets accessible as iterables
 from collections.abc import Sequence
@@ -49,24 +52,47 @@ class ExperimentData(Sequence):
     def append(self, d):
         self.data_dicts.append(d)
 
-def plot_data(dict_list: ExperimentData, finger="right", sensor_key='sensor_raw', mark10_key='mark10_raw'):
+def plot_data(dict_list: ExperimentData, finger="right", sensor_key='sensor_raw', mark10_key='mark10_raw', show=True):
     # Plot layout
     px = 1/plt.rcParams['figure.dpi']  # pixel in inches
     fig, axs = plt.subplots(2,7,figsize=(1920*px,1080*px))
     
     if finger == "right":
         for i in range(7):
-            # axs[0][i].plot(dict_list[i]['sensor_raw'][:,0],  dict_list[i]['sensor_raw'][:,-(i+1)]) # indexed from right with negative indices since csv has ordering reversed
-            axs[0][i].plot(dict_list[i][sensor_key][:,-(i+1)]) # indexed from right with negative indices since csv has ordering reversed
+            axs[0][i].plot(dict_list[i][sensor_key][:,0],  dict_list[i][sensor_key][:,-(i+1)]) # indexed from right with negative indices since csv has ordering reversed
+            # axs[0][i].plot(dict_list[i][sensor_key][:,-(i+1)]) # indexed from right with negative indices since csv has ordering reversed
             
-            # axs[1][i].plot(dict_list[i]['mark10_raw'][:,-1],  dict_list[i]['mark10_raw'][:,1])
-            axs[1][i].plot(dict_list[i][mark10_key][:,0],  dict_list[i][mark10_key][:,1])
+            axs[1][i].plot(dict_list[i][mark10_key][:,-1],  dict_list[i][mark10_key][:,1])
+            # axs[1][i].plot(dict_list[i][mark10_key][:,0],  dict_list[i][mark10_key][:,1])
     else:
         for i in range(7):
             axs[0][i].plot(dict_list[i][sensor_key][:,0],  dict_list[i][sensor_key][:,-(i+8)]) # again indexed from right with negative indices since csv has ordering reversed
             axs[1][i].plot(dict_list[i][mark10_key][:,-1],  dict_list[i][mark10_key][:,1])
-    plt.show()
+    if show:
+        plt.show()
+    return axs
+
+def plot_data_overlapping(dict_list: ExperimentData, finger="right", sensor_key='sensor_raw', mark10_key='mark10_raw', sensor_scale=1, show=True):
+    # Plot layout
+    px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+    fig, axs = plt.subplots(1,7,figsize=(1920*px,1080*px))
     
+    if finger == "right":
+        for i in range(7):
+            axs[i].plot(dict_list[i][sensor_key][:,0],  dict_list[i][sensor_key][:,-(i+1)]*sensor_scale, label="sensor") # indexed from right with negative indices since csv has ordering reversed
+            # axs[i].plot(dict_list[i][sensor_key][:,-(i+1)]*sensor_scale) # indexed from right with negative indices since csv has ordering reversed
+            
+            axs[i].plot(dict_list[i][mark10_key][:,-1],  dict_list[i][mark10_key][:,1], label="mark10")
+            # axs[i].plot(dict_list[i][mark10_key][:,0],  dict_list[i][mark10_key][:,1])
+    else:
+        for i in range(7):
+            axs[i].plot(dict_list[i][sensor_key][:,0],  dict_list[i][sensor_key][:,-(i+8)]) # again indexed from right with negative indices since csv has ordering reversed
+            axs[i].plot(dict_list[i][mark10_key][:,-1],  dict_list[i][mark10_key][:,1])
+    if show:
+        plt.legend()
+        plt.show()
+    return axs
+
 def read_tactile_data(filename:str, parent_directory=SENSOR_FILE_PATH) -> np.array:
     f = os.path.join(parent_directory,filename)
     table = pd.read_csv(f, skiprows=1)
@@ -128,7 +154,7 @@ if __name__ == "__main__":
     average_window = 1 # seconds
 
     for i in range(7):
-        data[i]["sensor_1pt"] = data[i]["sensor_raw"]
+        data[i]["sensor_1pt"] = data[i]["sensor_raw"].copy()
         data[i]["sensor_1pt"][:,1:] = data[i]["sensor_raw"][:,1:] - np.mean(data[i]["sensor_raw"][:sampling_freq*average_window,1:], axis=0)
 
     # Plot sensor data with 1pt calibration (to give delta)
@@ -148,24 +174,62 @@ if __name__ == "__main__":
     # 1. Time synch - Visually grab points where load starts getting registered by sensor and load cell
     # Store start of loading times in arrays to sync each experiment (1-7), 
     # Shift all data by these indices, plot to visually check front and back end alignment of load curves
-    sensor_start_idx = [57, 47, 70, 49, 44, 36, 33]
-    mark10_start_idx = [58, 49, 73, 53, 48, 34, 31]
+    sensor_start_idx = [58, 45, 69, 49, 44, 37, 33]
+    mark10_start_idx = [58, 49, 73, 51, 44, 34, 31]
     for i in range(7):
-        data[i]["sensor_1pt_synched"] = data[i]["sensor_1pt"][sensor_start_idx[i]:,:] # Only store values after synch point
+        data[i]["sensor_1pt_synched"] = data[i]["sensor_1pt"][sensor_start_idx[i]:,:].copy() # Only store values after synch point
         data[i]["sensor_1pt_synched"][:,0] = data[i]["sensor_1pt_synched"][:,0] - data[i]["sensor_1pt_synched"][0,0] # Set start of synched time to zero
 
-        data[i]["mark10_synched"] = data[i]["mark10_raw"][mark10_start_idx[i]:,:] # Only store values after synch point
+        data[i]["mark10_synched"] = data[i]["mark10_raw"][mark10_start_idx[i]:,:].copy() # Only store values after synch point
         data[i]["mark10_synched"][:,-1] = data[i]["mark10_synched"][:,-1] - data[i]["mark10_synched"][0,-1] # Set start of synched time to zero
         data[i]["mark10_synched"][:,0] = data[i]["mark10_synched"][:,0] - data[i]["mark10_synched"][0,0] 
 
-    
-    plot_data(data, sensor_key='sensor_1pt_synched', mark10_key='mark10_synched')    
+    # plot_data(data, sensor_key='sensor_1pt_synched', mark10_key='mark10_synched')    
+    # plot_data_overlapping(data, sensor_key='sensor_1pt_synched', mark10_key='mark10_synched', sensor_scale=1/2000)  # Scale sensor data to get a sense of time synchronicity between sensor and mark10 data
 
     # 2. Perform interpolation to get values from sensor and load cell at same timestamps
     # Use np.interp on both, input should be array of uniformly spaced times and outputs should be sensor readings and load cell readings
+    start_idx = 0
+    end_idx = 180
+
+    start_time = 0 / sampling_freq
+    end_time = 180 / sampling_freq
+
+    eval_times = np.arange(start_time, end_time, 1/sampling_freq)
+    
+
 
     # 3. Take interpolation outputs and use sensor readings as input (independent var) and load cell readings as output (dependent var).
     # Check over a couple of different polynomials, specifically cubic, quadratic, and linear.
+    coeffs = []
+    poly_order = 1
+    
+    for i in range(7):
+        c = poly.Polynomial.fit(data[i]['sensor_1pt_synched'][:end_idx,-(i+1)], data[i]['mark10_synched'][:end_idx,1], deg=poly_order)
+        coeffs.append(c)
+        
+        data[i]['sensor_cal_force'] = data[i]["sensor_1pt_synched"].copy()
+        data[i]['sensor_cal_force'][:,-(i+1)] = poly.polyval(data[i]["sensor_1pt_synched"][:,-(i+1)], c.convert().coef)
+
+    
+
+    px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+    fig, axs = plt.subplots(1,7,figsize=(1920*px,1080*px))
+    x = np.arange(0,15000,step=1)
+    for i in range(7):
+        axs[i].plot(data[i]['sensor_1pt_synched'][:int(end_idx/2),-(i+1)], data[i]['mark10_synched'][:int(end_idx/2),1], '--',color='red', label="Press")
+        axs[i].plot(data[i]['sensor_1pt_synched'][int(end_idx/2):end_idx,-(i+1)], data[i]['mark10_synched'][int(end_idx/2):end_idx,1], color='blue', label="release")
+        axs[i].plot(x, poly.polyval(x, coeffs[i].convert().coef), color="black", label="Fit function")
+
+        y = data[i]['mark10_synched'][:end_idx,1]
+        y_pred = poly.polyval(data[i]['sensor_1pt_synched'][:end_idx,-(i+1)], coeffs[i].convert().coef)
+        R2 = r2_score(y, y_pred)
+        axs[i].set_title("R^2 = {0}".format(np.round(R2,2)))
+        # axs[i].set_aspect('equal')
+    axs[i].legend()
+    plt.show()
+    
+    plot_data_overlapping(data, sensor_key='sensor_cal_force', mark10_key='mark10_synched')  # Scale sensor data to get a sense of time synchronicity between sensor and mark10 data
 
     # 4. Plot polyfit function with sensor inputs against load cell values to visually check match. Run R^2
 
